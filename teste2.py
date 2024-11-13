fe224b4076ce1e81100fdd2f3fa5333088f33699
AllezitoBR <alle_andrade123@hotmail.com>
2024-11-13 20:02:39 -0300

    quarto commit

---
+++ teste2.py
@@ -0,0 +1,7 @@
+cliente = input("Qual seu nome: ")
+cupom  = cliente.split()[0]
+compra = 500.00
+mes = ("MARÇO")
+
+
+print(f"Olá, {cliente}. Em {mes} você realizou uma compra no valor de R$ {compra} , e ganhou um desconto de 10% em sua proxima compra. use o cupom  {cupom} É10")
